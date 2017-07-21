b0450489a0790597208c705f1b053bf58b96c2c2
Maddie Stigler <mstigler@captechventures.com>
2017-07-20 23:33:32 -0400

    updated

--- Azure/azure-service/HTTPTrigger/index.js
+++ Azure/azure-service/HTTPTrigger/index.js
@@ -1,4 +1,3 @@
-// Please visit http://go.microsoft.com/fwlink/?LinkID=761099&clcid=0x409 for more information on settting up Github Webhooks
 module.exports = function (context, data) {
     context.log('GitHub Webhook triggered!', data.comment.body);
     context.res = { body: 'New GitHub pusht: ' + data.comment.body };
